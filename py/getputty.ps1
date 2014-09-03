0d9c15812c0199cbf63f8a3585f2d5ec49ca1470
jepio <jeremi.piotrowski@gmail.com>
2014-09-03 19:31:46 +0200

    Added readme and pshell script to get putty.

---
+++ py/getputty.ps1
@@ -0,0 +1,9 @@
+$addr = "http://the.earth.li/~sgtatham/putty/latest/x86/"
+$execs = @("pscp","putty","puttygen","pageant")
+$path = (Get-Item -Path ".\" -Verbose).FullName
+$client = new-object System.Net.WebClient
+
+foreach ($exec in $execs) {
+    echo "Getting $($addr)$($exec).exe"
+    $client.DownloadFile("$addr$exec.exe","$path\$exec.exe")
+}
