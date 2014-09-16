bbb075519d4c614e7e433618a7ad8071e9f2bf90
jepio <jeremi.piotrowski@gmail.com>
2014-09-16 15:29:55 +0200

    fixed script and made it download python as well

--- py/getputty.ps1
+++ py/getputty.ps1
@@ -1,9 +1,25 @@
+# the downloader object
+$client = new-object System.Net.WebClient
+# path to current directory
+$path = (Get-Item -Path ".\" -Verbose).FullName
+
+# downloading function
+function Download ($link,$file,$ext="exe")
+{
+    echo "Getting ${link}${file}.${ext}"
+    $client.DownloadFile("${link}/${file}.${ext}","${path}\${file}.${ext}")
+}
+
+# downloading putty components
 $addr = "http://the.earth.li/~sgtatham/putty/latest/x86/"
 $execs = @("pscp","plink","putty","puttygen","pageant")
-$path = (Get-Item -Path ".\" -Verbose).FullName
-$client = new-object System.Net.WebClient
 
 foreach ($exec in $execs) {
-    echo "Getting $($addr)$($exec).exe"
-    $client.DownloadFile("$addr$exec.exe","$path\$exec.exe")
+    Download -link $addr -file $exec
 }
+
+# download python
+$python_addr = "https://www.python.org/ftp/python/3.4.1/"
+$python_exec = "python-3.4.1"
+Download -link $python_addr -file $python_exec -ext "msi"
+
